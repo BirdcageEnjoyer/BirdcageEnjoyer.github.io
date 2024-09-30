de183f6dc5091a4834224c369ed134ca1c27aefe
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-09-30 18:48:20 +0100

    data structures worksheet task 2 q4

--- data structures/task 2 q4.py
+++ data structures/task 2 q4.py
@@ -1,2 +1,11 @@
-for quarter in range(0, 4):
-    w  
+import random
+
+
+outletSales = [random.randint(0, 51) for i in range(0, 4)]
+print(outletSales)
+
+for quarter in range(0,4):
+    total = []
+    for outlet in range(0, 50):
+        total[quarter] = outletSales[quarter][outlet]
+
